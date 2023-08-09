584e75f7542478515417aef7bfe16621d159cd7e
maksim_veremev <maksimbatuev1@gmail.com>
2023-08-09 13:06:37 +0300

    Нужно больше функций(изменения с EsLint)

--- js/functions.js
+++ js/functions.js
@@ -1,53 +1,46 @@
+/* eslint-disable no-unused-vars */
 //функция для проверки на палиндром
-const isPalindrom = (str) => {   
-    const tempStr = str
-        .toLowerCase()
-        .replaceAll(' ', '');
-    const halfStr = Math.floor(tempStr.length / 2);
+const isPalindrom = (str) => {
+  const tempStr = str
+    .toLowerCase()
+    .replaceAll(' ', '');
+  const halfStr = Math.floor(tempStr.length / 2);
 
-    for (let i = 0, j = -1; i <= halfStr; i++, j--) {
-        if (tempStr.at(i) !== tempStr.at(j)) {
-            return false;
-        }
+  for (let i = 0, j = -1; i <= halfStr; i++, j--) {
+    if (tempStr.at(i) !== tempStr.at(j)) {
+      return false;
     }
+  }
 
-    return true;
-}
-
-console.log(isPalindrom ('ДовОд'));
+  return true;
+};
 
 //функция по извлечению чисел и строки
-const extractNumber = (str) => { 
-    const pureStr = typeof str === 'number' ? String(str) : str;
-    let result = '';
-    
-    for (const char of pureStr) {
-        if (!Number.isNaN(parseInt(char, 10))) {
-            result += char;
-        }
+const extractNumber = (str) => {
+  const pureStr = typeof str === 'number' ? String(str) : str;
+  let result = '';
+
+  for (const char of pureStr) {
+    if (!Number.isNaN(parseInt(char, 10))) {
+      result += char;
     }
+  }
 
-    return parseInt(result, 10);
-}
-
-console.log(extractNumber('-1'));
+  return parseInt(result, 10);
+};
 
 //функция для проверки длины строки
 const isStrLengthEqual = (str, length) => str.length <= length;
 
-console.log(isStringLengthEqual('проверяемая строка', 20));
-
 //функция с добавлением символов к исходной строке
-const myPadStart = (string, minLength, pad) => {
-    let result = string;
+const padStart = (str, minLength, pad) => {
+  let result = str;
 
-    while (result.length < minLength) {
-        const newResultLength = result.length + pad.length;
-        const actualPad = newResultLength <= minLength ? pad : pad.slice(0, minLength - newResultLength);
-            result = actualPad + result;
-    }
-    
-    return result;
-}
+  while (result.length < minLength) {
+    const newResultLength = result.length + pad.length;
+    const actualPad = newResultLength <= minLength ? pad : pad.slice(0, minLength - newResultLength);
+    result = actualPad + result;
+  }
 
-console.log(myPadStart('q', 4, 'we'));
+  return result;
+};
