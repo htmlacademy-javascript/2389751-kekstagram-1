4937513702109a979d0319eef79d34fb5e46569f
maksim_veremev <maksimbatuev1@gmail.com>
2023-08-18 10:19:22 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+export {getRandomInteger, getRandomArrayElement};
