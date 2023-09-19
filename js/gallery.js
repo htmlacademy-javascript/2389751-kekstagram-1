8401151cd6a0e2d08956753eb8c91a80de7a1c08
maksim_veremev <maksimbatuev1@gmail.com>
2023-09-19 20:55:29 +0300

    Открывается и закрывается

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,44 @@
+import { showBigPicture } from './big-picture.js';
+
 const userPicture = document
   .querySelector('#picture')
   .content.querySelector('.picture');
-
 const gallery = document.querySelector('.pictures');
 
-const createTemplate = ({comments, description, likes, url}) => {
+const createTemplatePicture = ({comments, description, likes, url, id}) => {
   const templatePicture = userPicture.cloneNode(true);
 
   templatePicture.querySelector('.picture__img').src = url;
   templatePicture.querySelector('.picture__img').alt = description;
   templatePicture.querySelector('.picture__comments').textContent = comments.length;
   templatePicture.querySelector('.picture__likes').textContent = likes;
+  templatePicture.dataset.templatePictureId = id;
 
   return templatePicture;
 };
 
 export const renderGallery = (data) => {
   const fragment = document.createDocumentFragment();
-  data.forEach((picture) => {
-    const template = createTemplate(picture);
+
+  data.forEach ((picture) => {
+    const template = createTemplatePicture(picture);
     fragment.append(template);
   });
 
   gallery.append(fragment);
+};
+
+export const showGallery = (pictures) => {
+  gallery.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('data-template-picture-id');
+    if(!thumbnail) {
+      return;
+    }
+
+    const picture = pictures.find(
+      (item) => item.id === +thumbnail.dataset.templatePictureId
+    );
+    showBigPicture(picture);
+  });
+
 };
--- js/main.js
+++ js/main.js
@@ -2,6 +2,4 @@ import './utils.js';
 import {renderGallery} from './gallery.js';
 import {createGallery} from './data.js';
 
-const pictureGallery = createGallery(25);
-
-renderGallery(pictureGallery);
+renderGallery(createGallery());
